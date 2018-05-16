123e4b52d67d1e4abcfdba54f6dcb41bff5d61c8
Tyler Fox <tyler_fox@brown.edu>
2018-05-15 22:15:25 -0400

    updated iss tracker

--- js/main.js
+++ js/main.js
@@ -3,17 +3,46 @@ $("#pre-launch").hide();
 $("#pre-deployment").hide();
 $("#deployed").hide();
 
-const issIcon = "images/icons/iss-icon.png";
+const issIcon = "images/icons/iss-icon_white.png";
 
 let pvd, map, marker;
 let mapInited = false;
+
+var prevLatlng;
+
+function drawISSPath(latlng, map) {
+    if(prevLatlng == undefined) {
+        prevLatlng = latlng;
+    }
+    issPath = new google.maps.Polyline({
+        path: [prevLatlng, latlng],
+        strokeColor: "#a6c8de",
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    prevLatlng = latlng;
+    issPath.setMap(map);
+};
+
 // will be called when the maps API is initialized
-function initMap() {
+function initMap() {    
+
     pvd = new google.maps.LatLng(41.826684, -71.398011);
     // create map, centered on PVD in case of failure
     map = new google.maps.Map(document.getElementById('map'), {
         center: pvd,
-        zoom: 3
+        zoom: 4,   
+        minZoom: 3,
+        streetViewControl: false,
+        zoomControl: true,
+        fullscreenControl: false,
+        draggable: false,        
+        mapTypeControlOptions: {
+              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+              position: google.maps.ControlPosition.LEFT_BOTTOM
+        },
+        draggableCursor:'',
+        styles: [{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#19222a"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#2b343e"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#2b343e"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#2b343e"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#2b343e"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#257bcb"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#d7dee5"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#d7dee5"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#2b343e"},{"lightness":"52"},{"weight":"1"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#2b343e"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#2b343e"},{"lightness":"14"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#2b343e"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#2b343e"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#2b343e"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#19222a"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#19222a"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}]
     });
     // get ISS altitude and velocity
     $.get("https://api.wheretheiss.at/v1/satellites/25544", (res) => {
@@ -29,7 +58,7 @@ function initMap() {
             infowindow = new google.maps.InfoWindow({
                 content: `<p>This is the current location of the International Space Station.</p>
                 <p>EQUiSat is on board, awaiting deployment sometime early this summer.</p>
-                <p>The station is currently ${Math.floor(res.altitude)} km above sea level, traveling at ${Math.floor(res.velocity)} kmph.</p>`
+                <p>The station is currently ${Math.floor(res.altitude)} km above sea level, traveling at ${Math.floor(res.velocity)} km/hr.</p>`
             });
         }
         marker = new google.maps.Marker({
@@ -83,7 +112,8 @@ $('.js-tilt').tilt({
 });
 
 $(document).ready(() => {
-    let launch = new Date("2018-05-20T09:04:00");
+    let launch = new Date("2018-05-20T09:04:00");    
+
     // to see what the post-launch page will look like, uncomment the line below
     //launch = new Date("2018-04-03T00:00:01");
     let deployment = new Date("2018-06-20T09:04:00");
@@ -95,11 +125,8 @@ $(document).ready(() => {
             // pre-launch, hide everything other than the countdown
             $("#pre-launch").show();
         } else if (now >= launch && now < deployment) {
-            // pre-deployment, hide the countdown and the webapp
-            $("#title").html("EQUISAT HAS LAUNCHED");
-            $("#subtitle").html("EQUiSat has launched and is currently awaiting deployment on the International Space Station");
-            $("#pre-deployment").show();
-            $(".bg-img1")[0].style["background-image"] = "url('images/iss.jpg')";
+            // pre-deployment, hide the countdown and the webapp            
+            $("#pre-deployment").show();            
 
             // now continually update the location of the ISS on the map
             $.get("http://api.open-notify.org/iss-now.json", (res) => {
@@ -116,6 +143,7 @@ $(document).ready(() => {
                     if (mapInited) {
                         marker.setPosition(pos);
                         map.setCenter(pos);
+                        drawISSPath(pos, map)
                     }
                 }
             });
